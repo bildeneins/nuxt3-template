330699e82343f0547bbebf83f4c6d6e2e319ee03
Yajima Soichi <bildeneins@gmail.com>
2022-09-08 16:05:16 +0900

    Add SimpleButton.vue

---
+++ components/common/SimpleButton.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const props = defineProps<{primary?: boolean}>()
+const primary = !!props?.primary
+</script>
+
+<template>
+  <button
+    class="font-bold py-2 px-4 rounded border"
+    :class="{'text-white': primary, 'bg-blue-500': primary, 'hover:bg-blue-700': primary}"
+  >
+    <slot />
+  </button>
+</template>
